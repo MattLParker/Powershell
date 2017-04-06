14889bad6173a3dca7b5381fc9e39c3ee8d2d786
MattLParker <MLParker1@gmail.com>
2017-04-06 17:30:31 -0400

    Add files via upload

---
+++ Get SMB Shares from Servers.ps1
@@ -0,0 +1,3 @@
+﻿$computer=get-adcomputer -filter 'OperatingSystem -like "*server*"'|
+invoke-command -computername $computer.Name -scriptblock {Get-SmbShare}|
+export-csv -path SMBshares.csv -NoTypeInformation
---
+++ email Fix.ps1
@@ -0,0 +1,16 @@
+﻿# Get all mailboxes
+$Mailboxes = get-mailbox -ResultSize Unlimited;
+ 
+$Mailboxes | foreach{
+    for ($i=0;$i -lt $_.EmailAddresses.Count; $i++)
+    {
+        $address = $_.EmailAddresses[$i]
+        if ($address -like "*.us" -or $address -like "x500*" -or $address -like "x400*" -or $address -like "*.local" -or $address -like "ccmail*")
+        {
+            Write-host($address.AddressString.ToString() | out-file addressesRemoved.txt -append )
+            $_.EmailAddresses.RemoveAt($i)
+            $i--
+        }
+    }
+    Set-Mailbox -Identity $_.Identity -EmailAddresses $_.EmailAddresses
+}
