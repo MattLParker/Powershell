0b16c1c5aeacb7095b70e9fdc6563b60692bd0aa
Matt Parker <MLParker1@gmail.com>
2017-06-22 21:03:07 -0400

    Add 365 Mailbox

---
+++ All 365 Usermailbox Assign liscense.ps1
@@ -0,0 +1,21 @@
+﻿#Import Required PowerShell Modules
+Import-Module MSOnline
+
+#Connect to Office 365 
+$CloudUsername = 'Username'
+$pwdTxt = Get-Content "azure.txt"
+$securePwd = $pwdTxt | ConvertTo-SecureString 
+$CloudCred = New-Object System.Management.Automation.PSCredential $CloudUsername, $securePwd
+Connect-MsolService -Credential $CloudCred
+
+#Set Variables
+$L = New-MsolLicenseOptions -AccountSkuId default:ENTERPRISEPACK_GOV
+$usagelocation="US"
+$SKU="default:ENTERPRISEPACK_GOV"
+
+#The Magic Happens.
+Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox | Get-MsolUser | 
+foreach {set-msoluser -UserPrincipalName $_.UserPrincipalName -UsageLocation $UsageLocation
+        Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -addLicense $SKU
+        Set-MsolUserLicense -UserPrincipalName $_.UserPrincipalName -LicenseOptions $L}
+
