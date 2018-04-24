afe2325ecc5a2d72412d0bcf809c020a3a553603
mparker <mparker@maconbibb.us>
2018-04-24 11:29:42 -0400

    First upload of Backup User Script

---
+++ Backup-user.ps1
@@ -0,0 +1,69 @@
+$storage = "\\nas007\users\"
+$user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$user = $user.Replace("MACONBIBB\", "")
+$outname = $user + ".txt"
+$userprofile = $env:USERPROFILE
+$outfolder = $storage + $user
+New-Item -ItemType directory -Path $outfolder | Out-Null
+$out = $outfolder + "\" + $outname
+$Userout = "Username: " + $user
+$userout | out-file $out -Append   
+"
+Network Shares: "| out-file $out -Append   
+Get-CimInstance -Class Win32_NetworkConnection | where {$_.LocalName -ne $null} | Select-Object LocalName, RemoteName| out-file $out -Append   
+"
+Printers: "| out-file $out -Append 
+
+$fullprinters = get-printer 
+
+
+$Printers = $fullprinters -notmatch 'PDF|Onenote|WebEx|Microsoft XPS|Fax' | select-object Name, DriverName, PortName 
+
+$Printers | out-file $out -Append   
+"
+Taskbar: "| out-file $out -Append 
+
+$taskpath = $env:APPDATA + "\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+
+$Taskbar = get-childitem -path $taskpath | Foreach-Object {$_.BaseName}
+
+$Taskbar | out-file $out -Append
+
+
+"
+Programs: "| out-file $out -Append 
+
+
+$prog = @()
+$UninstallKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" 
+$reg = [microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine', $computername) 
+$regkey = $reg.OpenSubKey($UninstallKey) 
+$subkeys = $regkey.GetSubKeyNames() 
+foreach ($key in $subkeys) {
+
+    $thisKey = $UninstallKey + "\\" + $key 
+
+    $thisSubKey = $reg.OpenSubKey($thisKey) 
+
+    $obj = New-Object PSObject
+
+    $obj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $($thisSubKey.GetValue("DisplayName"))
+
+    $obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($thisSubKey.GetValue("DisplayVersion"))
+
+    $obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($thisSubKey.GetValue("Publisher"))
+
+    $prog += $obj
+
+} 
+
+
+$prog | Where-Object { $_.DisplayName } | select DisplayName, DisplayVersion, Publisher | Sort-object DisplayName |ft -auto | out-file $out -Append   
+
+#$chrome = $userprofile + "\AppData\Local\Google\Chrome\User Data\Default\"
+#Robocopy.exe "`"$chrome'"" "`"$outfolder`"" bookmarks*
+
+
+#WIP
+#robocopy.exe $source $dest /E /ZB /R:0 /W:0 /XJ /NFL /XD appdata OneDrive “Temporary Internet Files” OfficeFileCache Temp *cache* Spotify WER /XF *cache* *.ost
+
